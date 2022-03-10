c453df12829fa39a203a0a7ff7f4d89ec3c4caf6
Jamie Stackhouse <jamie.stackhouse@redspace.com>
2022-03-10 11:33:35 -0400

    Add jpeg support for image load.

--- js/webgl-texture-util.js
+++ js/webgl-texture-util.js
@@ -613,6 +613,8 @@ var WebGLTextureUtil = (function() {
         } else {
           gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
           gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         }
         var uploadTime = Date.now() - startTime;
 
